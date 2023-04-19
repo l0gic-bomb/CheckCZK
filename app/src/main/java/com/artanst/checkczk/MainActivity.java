963a5ad8e71f428d55e90eb7c0f01273fe9c08b0
Artem Krymov <artem.krymov@bk.ru>
2023-04-19 22:35:28 +0300

    add layout

--- app/src/main/java/com/artanst/checkczk/MainActivity.java
+++ app/src/main/java/com/artanst/checkczk/MainActivity.java
@@ -1,14 +1,105 @@
 package com.artanst.checkczk;
 
+import android.annotation.SuppressLint;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.*;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import com.artanst.checkczk.utility.Parser;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    public final String[] KEYS = {"AUD", "BGN", "BRL", "CAD", "CHF", "CNY", "DKK", "EUR", "GBP",
+            "HKD", "HUF", "IDR", "ILS", "INR", "ISK", "JPY", "KRW", "MXN", "MYR", "NOK", "NZD",
+            "PHP", "PLN", "RON", "SEK", "SGD", "THB", "TRY", "USD", "XDR", "ZAR"};
+
+    private DatePicker datePicker;
+    private TextView firstDate, secondDate, hint;
+    private Spinner currencies;
+    private Button reset, accept;
+
+    private Parser parser;
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        datePicker = findViewById(R.id.datePicker);
+        firstDate = findViewById(R.id.firstDate);
+        secondDate = findViewById(R.id.secnodDate);
+        hint = findViewById(R.id.hintTextView);
+        currencies = findViewById(R.id.currenciesSpinner);
+        accept = findViewById(R.id.acceptButton);
+        reset = findViewById(R.id.resetButton);
+
+        parser = new Parser();
+
+        setSpinner();
+        setAcceptListener();
+
+    }
+
+    private void parseData() {
+
+    }
+
+    private void setAcceptListener() {
+        accept.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String leftDate = firstDate.getText().toString();
+                String rightDate = secondDate.getText().toString();
+
+                int day = datePicker.getDayOfMonth();
+                int month = datePicker.getMonth() + 1;
+                int year = datePicker.getYear();
+
+                String date = String.valueOf(day) + "." + String.valueOf(month) + "." + String.valueOf(year);
+
+                if (leftDate.isEmpty())
+                {
+                    parser.leftYear = year;
+                    firstDate.setText(date);
+                    hint.setText("Введите 2 дату");
+                }
+                else if (rightDate.isEmpty()) {
+                    parser.rightYear = year;
+                    secondDate.setText(date);
+                    hint.setText("Нажмите потвердить для формирования отчета");
+                }
+                else {
+                    try {
+                        parser.parseRates();
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+            }
+        });
+    }
+
+    private void setResetListener() {
+
+    }
+
+    private void setSpinner() {
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, KEYS)  {
+            @Override
+            public View getView(int position, View convertView, ViewGroup parent) {
+                TextView view = (TextView) super.getView(position, convertView, parent);
+                view.setTextSize(36); // задаем размер текста
+                return view;
+            }
+        };
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        currencies.setAdapter(adapter);
     }
 }
--- app/src/main/java/com/artanst/checkczk/utility/Parser.java
+++ app/src/main/java/com/artanst/checkczk/utility/Parser.java
@@ -1,4 +1,48 @@
 package com.artanst.checkczk.utility;
 
+import com.artanst.checkczk.models.Rate;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+
 public class Parser {
+
+    public List<Rate> rates;
+    public int leftYear;
+    public int rightYear;
+
+    public Parser() {
+        rates = new ArrayList<>();
+    }
+
+    public void parseRates() throws IOException {
+        Executor executor = Executors.newSingleThreadExecutor();
+        String url = "https://www.cnb.cz/en/financial_markets/foreign_exchange_market/exchange_rate_fixing/year.txt?year=";
+        OkHttpClient client = new OkHttpClient();
+        executor.execute(new Runnable() {
+                             @Override
+                             public void run() {
+                                 for (int year = leftYear; year <= rightYear; ++year)
+                                 {
+                                     String tmpUrl = url + year;
+                                     Request request = new Request.Builder()
+                                             .url(tmpUrl)
+                                             .build();
+                                     Response response = null;
+                                     try {
+                                         response = client.newCall(request).execute();
+                                     } catch (IOException e) {
+                                         throw new RuntimeException(e);
+                                     }
+                                     String html = response.body().toString();
+                                 }
+                             }
+                         });
+    }
 }
